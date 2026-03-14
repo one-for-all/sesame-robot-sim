1b3a57d21d3148eb69d3eae94803b92e1ae1d5f7
Jay Li <jn.sapiens@gmail.com>
2026-03-14 20:23:56 +0900

    doc: add sesame code to git

---
+++ sesame/sesame.ino
@@ -0,0 +1,251 @@
+#include <Arduino.h>
+#include <ESP32Servo.h>
+#include "movement-sequences.h"
+
+// Global state for Animation
+String currentCommand = "";
+
+// ======================================================================
+// --- CONFIGURATION ---
+// ======================================================================
+Servo servos[8];
+
+// Sesame Distro Board Pinout
+const int servoPins[8] = {15, 2, 23, 19, 4, 16, 17, 18};
+
+// Subtrim values for each servo (offset in degrees)
+int8_t servoSubtrim[8] = {0, 0, 0, 0, 0, 0, 0, 0};
+
+// Animation constants
+int frameDelay = 100;
+int walkCycles = 10;
+int motorCurrentDelay = 20; // ms delay between motor movements to prevent over-current
+
+// Prototypes
+void setServoAngle(uint8_t channel, int angle);
+void enterIdle();
+void setFaceWithMode(const String& faceName, FaceAnimMode mode);
+void delayWithFace(unsigned long ms);
+bool pressingCheck(String cmd, int ms);
+void recordInput();
+
+// ======================================================================
+// --- SETUP ---
+// ======================================================================
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial);
+
+  Serial.println("-----------------------------------");
+  Serial.println("   Sesame Robot Simulation   ");
+  Serial.println("-----------------------------------");
+
+  // PWM Init
+  ESP32PWM::allocateTimer(0);
+  ESP32PWM::allocateTimer(1);
+  ESP32PWM::allocateTimer(2);
+  ESP32PWM::allocateTimer(3);
+
+
+  for (int i = 0; i < 8; i++) {
+    servos[i].setPeriodHertz(50);
+    // Map 0-180 to approx 732-2929us
+    servos[i].attach(servoPins[i], 732, 2929);
+  }
+  delay(10);
+}
+
+void loop() {
+  if (currentCommand != "") {
+    String cmd = currentCommand;
+    if (cmd == "forward") runWalkPose();
+    else if (cmd == "backward") runWalkBackward();
+    else if (cmd == "left") runTurnLeft();
+    else if (cmd == "right") runTurnRight();
+    else if (cmd == "rest") { runRestPose(); if (currentCommand == "rest") currentCommand = ""; }
+    else if (cmd == "stand") { runStandPose(1); if (currentCommand == "stand") currentCommand = ""; }
+    else if (cmd == "wave") runWavePose();
+    else if (cmd == "dance") runDancePose();
+    else if (cmd == "swim") runSwimPose();
+    else if (cmd == "point") runPointPose();
+    else if (cmd == "pushup") runPushupPose();
+    else if (cmd == "bow") runBowPose();
+    else if (cmd == "cute") runCutePose();
+    else if (cmd == "freaky") runFreakyPose();
+    else if (cmd == "worm") runWormPose();
+    else if (cmd == "shake") runShakePose();
+    else if (cmd == "shrug") runShrugPose();
+    else if (cmd == "dead") runDeadPose();
+    else if (cmd == "crab") runCrabPose();
+  }
+
+  // Serial CLI for debugging (can be used to diagnose servo position issues and wiring)
+  if (Serial.available()) {
+    static char command_buffer[32];
+    static byte buffer_pos = 0;
+    char c = Serial.read();
+    if (c == '\n' || c == '\r') {
+      if (buffer_pos > 0) {
+        command_buffer[buffer_pos] = '\0';
+        int motorNum, angle;
+        recordInput();
+        if(strcmp(command_buffer, "run walk") == 0 || strcmp(command_buffer, "rn wf") == 0) { currentCommand = "forward"; runWalkPose(); currentCommand = ""; }
+        else if(strcmp(command_buffer, "rn wb") == 0) { currentCommand = "backward"; runWalkBackward(); currentCommand = ""; }
+        else if(strcmp(command_buffer, "rn tl") == 0) { currentCommand = "left"; runTurnLeft(); currentCommand = ""; }
+        else if(strcmp(command_buffer, "rn tr") == 0) { currentCommand = "right"; runTurnRight(); currentCommand = ""; }
+        else if(strcmp(command_buffer, "run rest") == 0 || strcmp(command_buffer, "rn rs") == 0) runRestPose();
+        else if(strcmp(command_buffer, "run stand") == 0 || strcmp(command_buffer, "rn st") == 0) runStandPose(1);
+        else if(strcmp(command_buffer, "rn wv") == 0) { currentCommand = "wave"; runWavePose(); }
+        else if(strcmp(command_buffer, "rn dn") == 0) { Serial.println("I'm gonna dance"); currentCommand = "dance"; runDancePose(); }
+        else if(strcmp(command_buffer, "rn sw") == 0) { currentCommand = "swim"; runSwimPose(); }
+        else if(strcmp(command_buffer, "rn pt") == 0) { currentCommand = "point"; runPointPose(); }
+        else if(strcmp(command_buffer, "rn pu") == 0) { currentCommand = "pushup"; runPushupPose(); }
+        else if(strcmp(command_buffer, "rn bw") == 0) { currentCommand = "bow"; runBowPose(); }
+        else if(strcmp(command_buffer, "rn ct") == 0) { currentCommand = "cute"; runCutePose(); }
+        else if(strcmp(command_buffer, "rn fk") == 0) { currentCommand = "freaky"; runFreakyPose(); }
+        else if(strcmp(command_buffer, "rn wm") == 0) { currentCommand = "worm"; runWormPose(); }
+        else if(strcmp(command_buffer, "rn sk") == 0) { currentCommand = "shake"; runShakePose(); }
+        else if(strcmp(command_buffer, "rn sg") == 0) { currentCommand = "shrug"; runShrugPose(); }
+        else if(strcmp(command_buffer, "rn dd") == 0) { currentCommand = "dead"; runDeadPose(); }
+        else if(strcmp(command_buffer, "rn cb") == 0) { currentCommand = "crab"; runCrabPose(); }
+        else if (strcmp(command_buffer, "subtrim") == 0 || strcmp(command_buffer, "st") == 0) {
+          Serial.println("Subtrim values:");
+          for (int i = 0; i < 8; i++) {
+            Serial.print("Motor "); Serial.print(i); Serial.print(": ");
+            if (servoSubtrim[i] >= 0) Serial.print("+");
+            Serial.println(servoSubtrim[i]);
+          }
+        }
+        else if (strcmp(command_buffer, "subtrim save") == 0 || strcmp(command_buffer, "st save") == 0) {
+          Serial.println("Copy and paste this into your code:");
+          Serial.print("int8_t servoSubtrim[8] = {");
+          for (int i = 0; i < 8; i++) {
+            Serial.print(servoSubtrim[i]);
+            if (i < 7) Serial.print(", ");
+          }
+          Serial.println("};");
+        }
+        else if (strncmp(command_buffer, "subtrim reset", 13) == 0 || strncmp(command_buffer, "st reset", 8) == 0) {
+          for (int i = 0; i < 8; i++) servoSubtrim[i] = 0;
+          Serial.println("All subtrim values reset to 0");
+        }
+        else if (strncmp(command_buffer, "subtrim ", 8) == 0 || strncmp(command_buffer, "st ", 3) == 0) {
+          const char* params = (command_buffer[1] == 't') ? command_buffer + 3 : command_buffer + 8;
+          int trimMotor, trimValue;
+          if (sscanf(params, "%d %d", &trimMotor, &trimValue) == 2) {
+            if (trimMotor >= 0 && trimMotor < 8) {
+              if (trimValue >= -90 && trimValue <= 90) {
+                servoSubtrim[trimMotor] = trimValue;
+                Serial.print("Motor "); Serial.print(trimMotor); Serial.print(" subtrim set to ");
+                if (trimValue >= 0) Serial.print("+");
+                Serial.println(trimValue);
+              } else {
+                Serial.println("Subtrim value must be between -90 and +90");
+              }
+            } else {
+              Serial.println("Invalid motor number (0-7)");
+            }
+          }
+        }
+        else if (strncmp(command_buffer, "all ", 4) == 0) {
+              if (sscanf(command_buffer + 4, "%d", &angle) == 1) {
+                  for (int i = 0; i < 8; i++) setServoAngle(i, angle);
+                  Serial.print("All servos set to "); Serial.println(angle);
+              }
+        }
+        else if (sscanf(command_buffer, "%d %d", &motorNum, &angle) == 2) {
+              if (motorNum >= 0 && motorNum < 8) {
+                  setServoAngle(motorNum, angle);
+                  Serial.print("Servo "); Serial.print(motorNum); Serial.print(" set to "); Serial.println(angle);
+              } else {
+                  Serial.println("Invalid motor number (0-7)");
+              }
+        }
+        buffer_pos = 0;
+      }
+    } else if (buffer_pos < sizeof(command_buffer) - 1) {
+      command_buffer[buffer_pos++] = c;
+    }
+  }
+
+  // runRestPose();
+  // runStandPose(1);
+  // runWavePose();
+  // runSwimPose();
+  // runPointPose();
+  // runPushupPose();
+  // runBowPose();
+  // runWormPose();
+  // runShakePose();
+  // runCrabPose();
+  // runDeadPose();
+  // runWalkPose();
+  // runWalkBackward();
+  // runTurnLeft();
+  // runTurnRight();
+
+  // The following are not really working. I need to add more collision points for them to look right. But you can still try these out for amusement :)
+  // runShrugPose();
+  // runCutePose();
+  // runFreakyPose();
+  // runDancePose();
+}
+
+
+// ====== HELPERS ======
+// commented out the lines that are not implemented yet
+void setServoAngle(uint8_t channel, int angle) {
+  if (channel < 8) {
+    int adjustedAngle = constrain(angle + servoSubtrim[channel], 0, 180);
+    servos[channel].write(adjustedAngle);
+    delayWithFace(motorCurrentDelay);
+  }
+}
+
+void delayWithFace(unsigned long ms) {
+  unsigned long start = millis();
+  while (millis() - start < ms) {
+    // updateAnimatedFace();
+    // server.handleClient();
+    // dnsServer.processNextRequest();
+    delay(5);
+  }
+}
+
+
+void setFaceWithMode(const String& faceName, FaceAnimMode mode) {
+  // setFaceMode(mode);
+  // setFace(faceName);
+}
+
+void enterIdle() {
+  // idleActive = true;
+  // idleBlinkActive = false;
+  // idleBlinkRepeatsLeft = 0;
+  // setFaceWithMode("idle", FACE_ANIM_BOOMERANG);
+  // scheduleNextIdleBlink(3000, 7000);
+}
+
+bool pressingCheck(String cmd, int ms) {
+  unsigned long start = millis();
+  while (millis() - start < ms) {
+    // server.handleClient();
+    // dnsServer.processNextRequest();
+    // updateAnimatedFace();
+    // if (currentCommand != cmd) {
+    //   runStandPose(1);
+    //   return false;
+    // }
+    yield();
+  }
+  return true;
+}
+
+void recordInput() {
+  // lastInputTime = millis();
+  // if (!firstInputReceived) {
+  //   firstInputReceived = true;
+  //   showingWifiInfo = false;
+  // }
+}
